ff77ac842ae7298ef34e4e5f637ea333f147a58e
Flavio Vilante <fvilante1@gmail.com>
2025-02-24 22:38:43 -0300

    Feat: Adicionado cores as mensagens de log na tela.

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,27 @@ use clap::Parser;
 use opencv::{core, highgui, imgcodecs, imgproc, prelude::*, videoio, Result};
 use prettytable::{color, Attr, Cell, Row, Table};
 
-use fern::Dispatch;
+use fern::colors::{Color, ColoredLevelConfig};
 use log::{debug, error, info, trace, warn};
 use std::{io, time::SystemTime};
 
 fn setup_logging(config: &Config) -> Result<(), fern::InitError> {
-    let mut base_config = fern::Dispatch::new();
+    // configure colors for the whole line
+    let colors_line = ColoredLevelConfig::new()
+        .error(Color::Red)
+        .warn(Color::Yellow)
+        // we actually don't need to specify the color for debug and info, they are white by default
+        .info(Color::White)
+        .debug(Color::White)
+        // depending on the terminals color scheme, this is the same as the background color
+        .trace(Color::BrightBlack);
 
-    let x = config.log_file;
+    // configure colors for the name of the level.
+    // since almost all of them are the same as the color for the whole line, we
+    // just clone `colors_line` and overwrite our changes
+    let colors_level = colors_line.info(Color::Green);
+
+    let mut base_config = fern::Dispatch::new();
 
     // Se o usuário passou explicitamente --log-file, adicionamos log para arquivo
     if config.log_file {
@@ -34,24 +47,26 @@ fn setup_logging(config: &Config) -> Result<(), fern::InitError> {
     // Configuração do log para stdout
     let stdout_config = fern::Dispatch::new()
         .level(log::LevelFilter::Info)
-        .format(|out, message, record| {
-            if record.level() > log::LevelFilter::Info && record.target() == "cmd_program" {
-                out.finish(format_args!(
-                    "DEBUG @ {}: {}",
-                    humantime::format_rfc3339_seconds(SystemTime::now()),
-                    message
-                ))
-            } else {
-                out.finish(format_args!(
-                    "[{} {} {}] {}",
-                    humantime::format_rfc3339_seconds(SystemTime::now()),
-                    record.level(),
-                    record.target(),
-                    message
-                ))
-            }
+        .format(move |out, message, record| {
+            out.finish(format_args!(
+                "{color_line}[{date} {level} {target} {color_line}] {message}\x1B[0m",
+                color_line = format_args!(
+                    "\x1B[{}m",
+                    colors_line.get_color(&record.level()).to_fg_str()
+                ),
+                date = humantime::format_rfc3339_seconds(SystemTime::now()),
+                target = record.target(),
+                level = colors_level.color(record.level()),
+                message = message,
+            ));
         })
-        .chain(io::stdout());
+        // change log levels for individual modules. Note: This looks for the record's target
+        // field which defaults to the module path but can be overwritten with the `target`
+        // parameter:
+        // `info!(target="special_target", "This log message is about special_target");`
+        .level_for("pretty_colored", log::LevelFilter::Trace)
+        // output to stdout
+        .chain(std::io::stdout());
 
     // Aplica a configuração de logging
     base_config.chain(stdout_config).apply()?;
